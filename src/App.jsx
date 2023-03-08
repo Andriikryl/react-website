186f33520334082e665c303e60cb3b7b70d98db0
Amdrii <andreyk557@gmail.com>
2023-03-08 13:54:23 +0200

    git[init]

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Contact from './components/Contact'
+import Hero from './components/Hero'
+import Who from './components/Who'
+import Works from './components/Works'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Hero/>
+      <Who/>
+      <Works/>
+      <Contact/>
+    </div>
+  )
+}
+
+export default App
